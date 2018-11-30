8fa45ee5bacd40217dbbf913e9804dbc3eb41bf0
t00189412 <ronan.parkinson@students.ittralee.ie>
2018-11-30 17:44:52 +0000

    All outputs are in this class

--- showOrder.java
+++ showOrder.java
@@ -1,14 +1,36 @@
+import java.util.LinkedList;
+import java.util.List;
+
 public class showOrder {
     public static void main(String[] args) {
         newOrder fillorder = new fillOrder();
 
-        //Using different methods were as the example uses the same method
-        //maybe hae more then one drink
-        //fillorder.setOrder(new Chips());
-       // fillorder.include();
         fillorder.setOrder(new Chips());
         fillorder.include();
         fillorder.setOrder(new Drink());
         fillorder.include();
+
+        ConcreteBurgerList concreteBurgerList = new ConcreteBurgerList();
+        ConcreteBurgerScreen concreteBurgerScreen = new ConcreteBurgerScreen();
+        concreteBurgerScreen.display(concreteBurgerList);
+        concreteBurgerList.addBurger(concreteBurgerScreen);
+        System.out.println("Added a new Burger!");
+        System.out.println(concreteBurgerList.getBurger());
+        concreteBurgerList.notifyBurger();
+        concreteBurgerList.removeBurger(concreteBurgerScreen);
+        System.out.println(concreteBurgerList.getBurger());
+
+        Customer customer = new Customer();
+        System.out.println(customer.burger());
+
+        Choice selection = makeDessert.getOption("dessert");
+        dessert des = selection.getDessert("muffin");
+        des.create();
+
+        Choice selectionTwo = makeDessert.getOption("topping");
+        Topping topping = selectionTwo.getExtra("chocolate");
+        topping.extra();
+        
+        System.out.println(orderCost.getInstance().toString());
     }
 }
